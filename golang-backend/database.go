58bed8b878bb4e5c60fa419a9162217917321367
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-11-09 14:31:48 +0200

    Create Tables
    this function in the db file creates the tables and the columns within the DB

--- golang-backend/database.go
+++ golang-backend/database.go
@@ -25,3 +25,36 @@ func InitializeDB() {
 
 	DB = db
 }
+
+func CreateTables() {
+	_, err := DB.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			userid INT AUTO_INCREMENT PRIMARY KEY,
+			name VARCHAR(50) NOT NULL,
+			email VARCHAR(50) NOT NULL,
+			password VARCHAR(50) NOT NULL,
+			usertype ENUM('doctor', 'patient') NOT NULL
+		)
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = DB.Exec(`
+		CREATE TABLE IF NOT EXISTS appointments (
+			appointment_id INT AUTO_INCREMENT PRIMARY KEY,
+			doctor_id INT,
+			patient_id INT,
+			appointment_date DATE NOT NULL,
+			start_time TIME NOT NULL,
+			end_time TIME NOT NULL,
+			FOREIGN KEY (doctor_id) REFERENCES users(userid),
+			FOREIGN KEY (patient_id) REFERENCES users(userid)
+		)
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Tables created successfully")
+}
