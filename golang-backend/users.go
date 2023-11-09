3eb47a5eb83b6d784889907d96c964bb19f302e9
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-11-09 14:39:30 +0200

    Updates to File Structure
    returned user.go to the main package with some updates

--- golang-backend/users.go
+++ golang-backend/users.go
@@ -1,18 +1,19 @@
-package user
+package main
 
 import "database/sql"
 
 // User represents a user in the system.
 type User struct {
-	ID       int
-	Username string
+	UserID   int
+	Name     string
 	Email    string
+	Password string
 	UserType string
 }
 
 // CreateUser inserts a new user into the database.
 func CreateUser(db *sql.DB, user *User) error {
-	_, err := db.Exec("INSERT INTO users (username, email, user_type) VALUES (?, ?, ?)", user.Username, user.Email, user.UserType)
+	_, err := db.Exec("INSERT INTO users (username, email, user_type) VALUES (?, ?, ?)", user.Name, user.Email, user.UserType)
 	if err != nil {
 		return err
 	}
@@ -22,7 +23,7 @@ func CreateUser(db *sql.DB, user *User) error {
 // GetUserByID retrieves a user by their ID.
 func GetUserByID(db *sql.DB, userID int) (*User, error) {
 	user := new(User)
-	err := db.QueryRow("SELECT id, username, email, user_type FROM users WHERE id = ?", userID).Scan(&user.ID, &user.Username, &user.Email, &user.UserType)
+	err := db.QueryRow("SELECT id, username, email, user_type FROM users WHERE id = ?", userID).Scan(&user.UserID, &user.Name, &user.Email, &user.UserType)
 	if err != nil {
 		return nil, err
 	}
