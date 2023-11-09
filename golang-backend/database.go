852d4e98ba6448ef8e4895a8c64b96626bf1368c
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-11-09 14:27:21 +0200

    Update database.go
    initialize db function completed to open the connection between the Go back end and the DB

--- golang-backend/database.go
+++ golang-backend/database.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
+
+func InitializeDB() {
+	db, err := sql.Open("mysql", "root:7R26@llg4grb$&@tcp(127.0.0.1:3306)/Clinical_Reservation")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to the database")
+
+	DB = db
+}
