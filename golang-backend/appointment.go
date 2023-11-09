586e09d3a27dae63b13eab48ba16e45ee824a1ea
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-11-09 19:14:51 +0200

    SetDoctor Schedule
    the function to add the slot for the doctor

--- golang-backend/appointment.go
+++ golang-backend/appointment.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -11,4 +12,47 @@ type Appointment struct {
 	AppointmentDate time.Time
 	StartTime       time.Time
 	EndTime         time.Time
+}
+
+// SetDoctorSchedule sets the schedule for a doctor
+func SetDoctorSchedule(doctorID int, appointmentDate time.Time, startTime time.Time, endTime time.Time) error {
+	// Check if the slot is already occupied
+	isSlotOccupied, err := IsSlotOccupied(doctorID, appointmentDate, startTime, endTime)
+	if err != nil {
+		return err
+	}
+
+	if isSlotOccupied {
+		return fmt.Errorf("the slot is already occupied")
+	}
+
+	// If the slot is not occupied, insert the new slot
+	_, err = DB.Exec(`
+		INSERT INTO appointments (doctor_id, appointment_date, start_time, end_time) 
+		VALUES (?, ?, ?, ?)
+	`, doctorID, appointmentDate, startTime, endTime)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// IsSlotOccupied checks if the slot is already occupied
+func IsSlotOccupied(doctorID int, appointmentDate time.Time, startTime time.Time, endTime time.Time) (bool, error) {
+	var count int
+	err := DB.QueryRow(`
+		SELECT COUNT(*) 
+		FROM appointments 
+		WHERE doctor_id = ? 
+		AND appointment_date = ? 
+		AND ((start_time <= ? AND end_time >= ?) OR (start_time <= ? AND end_time >= ?))
+	`, doctorID, appointmentDate, startTime, startTime, endTime, endTime).Scan(&count)
+
+	if err != nil {
+		return false, err
+	}
+
+	return count > 0, nil
 }
