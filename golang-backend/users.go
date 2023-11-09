aa242ba5debc608c6c67ea65cd60c8ecdb81411f
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-11-09 18:17:47 +0200

    Sign Up User
    The sign-up user function is the user.go to add a new user to the system

--- golang-backend/users.go
+++ golang-backend/users.go
@@ -1,7 +1,5 @@
 package main
 
-import "database/sql"
-
 // User represents a user in the system.
 type User struct {
 	UserID   int
@@ -11,21 +9,11 @@ type User struct {
 	UserType string
 }
 
-// CreateUser inserts a new user into the database.
-func CreateUser(db *sql.DB, user *User) error {
-	_, err := db.Exec("INSERT INTO users (username, email, user_type) VALUES (?, ?, ?)", user.Name, user.Email, user.UserType)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+func SignUp(user User) error {
+	_, err := DB.Exec(`
+		INSERT INTO users (name, email, password, usertype) 
+		VALUES (?, ?, ?, ?)
+	`, user.Name, user.Email, user.Password, user.UserType)
 
-// GetUserByID retrieves a user by their ID.
-func GetUserByID(db *sql.DB, userID int) (*User, error) {
-	user := new(User)
-	err := db.QueryRow("SELECT id, username, email, user_type FROM users WHERE id = ?", userID).Scan(&user.UserID, &user.Name, &user.Email, &user.UserType)
-	if err != nil {
-		return nil, err
-	}
-	return user, nil
+	return err
 }
