fcc16d55215f10bccf9fbef1810bb422b87ddb3a
Ahmed Elsayed <ahmed33elsayed22@gmail.com>
2023-11-09 18:46:37 +0200

    Update appointment.go
    Added the Appointment struct to represent the appointments in the system

--- golang-backend/appointment.go
+++ golang-backend/appointment.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"time"
+)
+
+type Appointment struct {
+	AppointmentID   int
+	DoctorID        int
+	PatientID       int
+	AppointmentDate time.Time
+	StartTime       time.Time
+	EndTime         time.Time
+}
